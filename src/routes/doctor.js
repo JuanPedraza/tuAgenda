84b62b857e2eaa3c68a399b3f0a0f4bebd2c4393
Juan Pedraza <juan_pedraza@outlook.com>
2021-11-08 21:10:41 -0500

    Crud Completed

--- src/routes/doctor.js
+++ src/routes/doctor.js
@@ -8,15 +8,51 @@ const router = express.Router()
 
 // Crear Médico
 
-router.post("/createDoctor", (req,res)=>
+router.post("/doctor", (req,res)=>
 {
     const doctor = doctorSchema(req.body)
     doctor.save().then((data) => res.json(data)).catch((err) => res.json({message: err}))
 
 })
 
-router.get("/listarDoctor", (req,res)=>{
-    
+// Listar toda la base
+router.get("/doctor", (req,res)=>{
+    doctorSchema.find().then((data) => res.json(data)).catch((err) => res.json({message: err}))
 })
+
+// Listar por id
+router.get("/doctor/:id", (req,res)=>
+{
+    const { id } = req.params
+    doctorSchema.findById(id).then((data) => res.json(data)).catch((err) => res.json({message: err}))    
+})
+
+// Actualizar registro
+
+router.put("/doctor/:id", (req,res)=>
+{
+    const { id } = req.params
+    const {nombre,especialidad,documento,tel,direccion,email} = req.body
+    doctorSchema
+        .updateOne({ _id: id },{ $set: {nombre,especialidad,documento,tel,direccion,email}})
+        .then((data) => res.json(data))
+        .catch((err) => res.json({message: err}))    
+})
+
+router.delete("/doctor/:id", (req,res)=>
+{
+    const { id } = req.params
+    
+    doctorSchema
+        .deleteOne({ _id: id })
+        .then((data) => res.json(data))
+        .catch((err) => res.json({message: err}))    
+})
+
+
+
+
+
+
 
 module.exports = router
